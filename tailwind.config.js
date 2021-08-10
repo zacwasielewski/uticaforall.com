bdcdd0278b6b47451c0e0768c29a526cd90b145b
Zac Wasielewski <zac@wasielewski.org>
2021-08-09 23:33:41 -0400

    Big WIP!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: {
     mode: "all",
@@ -10,10 +13,45 @@ module.exports = {
     container: {
       center: true,
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      purple: {
+        DEFAULT: '#52128d',
+        '50':  '#f9fafb',
+        '100': '#eceff9',
+        '200': '#dedaf1',
+        '300': '#cdb9f6',
+        '400': '#b993f0',
+        '500': '#a16de4',
+        '600': '#894acd',
+        '700': '#712bae',
+        '800': '#52128d',
+        '900': '#200769',
+      }
+    },
+    textIndent: theme => theme('spacing'),
+    
+    textIndent: (theme, { negative }) => ({
+      ...theme('spacing'),
+      ...negative(theme('spacing')),
+    }),
+
     extend: {
-      colors: {},
+      fontFamily: {
+        "display": ["raleway", ...defaultTheme.fontFamily.sans],
+        "sans": [ "trade-gothic-next", ...defaultTheme.fontFamily.sans],
+        "serif": [ "source-serif-4", ...defaultTheme.fontFamily.serif],
+      },
     },
   },
   variants: {},
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('tailwindcss-text-indent')(),
+  ],
 };
